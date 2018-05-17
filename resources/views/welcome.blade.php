9c481ee41c19f2f98651b2ce6a65e3cfc33ff80e
zhangwei <50098161@qq.com>
2018-05-17 15:49:25 +0000

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Hello Laravel! - by Summer</div>
+        <div class="title">Hello Laravel! - by Summer2</div>
     </div>
 </div>
 </body>
